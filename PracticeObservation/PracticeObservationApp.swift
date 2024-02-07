de5dfdcda1c382bbd46054c8aeb4f11738db297b
Rin-t <rin.t.4921@gmail.com>
2024-02-07 19:45:28 +0900

    Initial Commit

---
+++ PracticeObservation/PracticeObservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeObservationApp.swift
+//  PracticeObservation
+//
+//  Created by Rin on 2024/02/07.
+//
+
+import SwiftUI
+
+@main
+struct PracticeObservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
